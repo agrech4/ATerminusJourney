d886c094d1e6e0a6e58f618a36193e7b444b4b1d
Alexander Grech IV <alexiv42@gmail.com>
2019-11-14 19:23:54 -0500

    Reorginization of text folder and other stuff

---
+++ ATerminusJourneyProject/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+    public string sceneToLoad;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void setScene(string scene) {
+        sceneToLoad = scene;
+    }
+    
+    private void loadScene() {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void OnTriggerStay2D(Collider2D other) {
+        float enter = Input.GetAxis("Submit");
+        if (other.CompareTag("Player") && (enter > 0) ) {
+            loadScene();
+        }
+    }
+}
