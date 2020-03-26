239e508390297458a74bf79f2ca897652bc630ed
Alexander Grech IV <alexiv42@gmail.com>
2020-03-26 14:56:21 -0500

    Menu animations

--- ATerminusJourneyProject/Assets/Scripts/EncounterMenu.cs
+++ ATerminusJourneyProject/Assets/Scripts/EncounterMenu.cs
@@ -6,14 +6,19 @@ using TMPro;
 public class EncounterMenu : MonoBehaviour
 {
 
-    public GameObject encounterMenu;
+    public GameObject Canvas;
     public PlayerData playerData;
     public TMP_Text playerName;
+    private Animator animator;
+    private bool activeMenu = true;
+    private bool menuWait = false;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = Canvas.GetComponent<Animator>();
         playerName.text = playerData.charName;
     }
 
@@ -30,8 +35,19 @@ public class EncounterMenu : MonoBehaviour
 
 
     void ToggleMenu() {
-        encounterMenu.SetActive(!encounterMenu.activeInHierarchy);
+        if (!menuWait) {
+            activeMenu = !activeMenu;
+            float animationTime = animator.GetCurrentAnimatorClipInfo(0)[0].clip.length;
+            animator.SetBool("activeMenu", activeMenu);
+            //float animationTime = animator.GetCurrentAnimatorStateInfo(0).
+            StartCoroutine(WaitForSlide());
+        }
     }
 
+    private IEnumerator WaitForSlide() {
+        menuWait = true;
+        yield return new WaitForSeconds(.7f);
+        menuWait = false;
+    }
 
 }
